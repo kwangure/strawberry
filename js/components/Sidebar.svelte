7b6f85ae79552856030aeead625d89ea152d4bc4
kwangure <kaf.wangure@gmail.com>
2019-08-26 15:26:50 +0300

    feat: add sidebar component.

--- js/components/Sidebar.svelte
+++ js/components/Sidebar.svelte
@@ -8,11 +8,17 @@
     <div class="sidebar-head">
         <slot name="head"></slot>
     </div>
-    {#each items as item, index}
-        <div class="sidebar-item {item.active ?'active':''}">
-            <slot name="item" item={{...item, index}}></slot>
-        </div>
-    {/each}
+    <div class="sidebar-items">
+        {#each items as item, index}
+        
+            <div class="sidebar-item {item.active ?'active':''}">
+                <slot name="item" item={{...item, index}}></slot>
+            </div>
+        {/each}
+    </div>
+    <div class="sidebar-footer">
+        <slot name="foot"></slot>
+    </div>
 </div>
 
 <style>
@@ -20,6 +26,15 @@
         width: 240px;
         padding: 0 10px;
         height: 100%;
+        display: flex;
+        flex-direction: column;
+    }
+    .sidebar-items {
+        overflow: hidden;
+        overflow-y: scroll;
+    }
+    .sidebar-footer {
+        margin-top: auto;
     }
     .sidebar-item {
         cursor: pointer;
@@ -35,17 +50,14 @@
         text-decoration: none;
         padding: 8px 12px;
     }
-    .sidebar-item.icon {
-        padding-left: 30px;
-    }
     .sidebar-item:not(.active):hover {
         background-color: #f9f9f9;
     }
-    .sidebar-item > :global(*:active),
     .sidebar-item.active {
         background-color: #e8f0fe;
-    }
-    .sidebar-item.active > :global(*) {
         color:#2885ff;
+    }
+    .sidebar-item + .sidebar-item {
+        margin-top: 3px;
     }
 </style>
