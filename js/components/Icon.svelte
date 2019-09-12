8e0c254a4c3884474b6965a65d1181a4396ed8a4
kwangure <kaf.wangure@gmail.com>
2019-09-12 12:07:49 +0300

    feat: add flip and spin to icon

--- js/components/Icon.svelte
+++ js/components/Icon.svelte
@@ -1,20 +1,94 @@
 <script>
     export let size = 24
     export let path = ''
+    export let flip = ''
+    export let spin = ''
     //TODO
     export let color = null
     export let rotate = 0
-    export let spin = false
-    export let flip = false
 
-    if(flip != 'h' && flip != 'v') {
-        flip = false
+    function validSpin(spin) {
+        return spin === 'clockwise' || spin === 'counterclockwise'
     }
+
+    function validFlip(spin) {
+        return flip === 'horizontal' || flip === 'vertical'
+    }
+
+    $: horizontal = validFlip(flip) && flip === 'horizontal'
+    $: vertical = validFlip(flip) && flip === 'vertical'
+    $: clockwise = validSpin(spin) && spin === 'clockwise'
+    $: counterclockwise = validSpin(spin) && spin === 'counterclockwise'
 </script>
 
 <svelte:options tag="sb-icon"/>
 
-<svg height="{size}px" on:click on:dblclick viewBox="0 0 24 24" 
-    width="{size}px">
+<svg class:horizontal class:vertical class:clockwise class:counterclockwise
+    height="{size}px" 
+    on:click on:dblclick viewBox="0 0 24 24" width="{size}px">
     <path d={path} fill="currentColor" />
 </svg>
+
+<style>
+    svg.horizontal {
+        transform: scale(-1, 1)
+    }
+    svg.vertical {
+        transform: scale(1, -1)
+    }
+
+    svg.clockwise {
+        -webkit-animation: clockwise 2s infinite linear;
+        animation: clockwise 2s infinite linear;
+    }
+
+    @-webkit-keyframes clockwise {
+        0% {
+            -webkit-transform: rotate(0deg);
+            transform: rotate(0deg);
+        }
+        100% {
+            -webkit-transform: rotate(359deg);
+            transform: rotate(359deg);
+        }
+    }
+
+    @keyframes clockwise {
+        0% {
+            -webkit-transform: rotate(0deg);
+            transform: rotate(0deg);
+        }
+        100% {
+            -webkit-transform: rotate(359deg);
+            transform: rotate(359deg);
+        }
+    }
+
+    svg.counterclockwise {
+        -webkit-animation: counterclockwise 2s infinite linear;
+        animation: counterclockwise 2s infinite linear;
+    }
+
+    @-webkit-keyframes counterclockwise {
+        0% {
+            -webkit-transform: rotate(359deg);
+            transform: rotate(359deg);
+        }
+        100% {
+            -webkit-transform: rotate(0deg);
+            transform: rotate(0deg);
+        }   
+    }
+
+    @keyframes counterclockwise {
+        0% {
+            -webkit-transform: rotate(359deg);
+            transform: rotate(359deg);
+        }
+        100% {
+            -webkit-transform: rotate(0deg);
+            transform: rotate(0deg);
+        }
+    }
+
+</style>
