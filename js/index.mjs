27b0d8336481eab032400d4ca1610fba8a6c6666
kwangure <kaf.wangure@gmail.com>
2019-09-27 22:03:28 +0300

    feat: add number input component

--- js/index.mjs
+++ js/index.mjs
@@ -1,9 +1,14 @@
+import Input from './components/Input.svelte'
+import InputNumber from './components/InputNumber.svelte'
+
+Input.Number = InputNumber
+export { default as Input }
+
 export { default as Button } from './components/Button.svelte'
 export { default as ButtonGroup } from './components/ButtonGroup.svelte'
 export { default as ButtonTabs } from './components/ButtonTabs.svelte'
 export { default as Dropdown } from './components/Dropdown.svelte'
 export { default as Icon } from './components/Icon.svelte'
-export { default as Input } from './components/Input.svelte'
 export { default as Modal } from './components/Modal.svelte'
 export { default as Sidebar } from './components/Sidebar.svelte'
 export { default as Textarea } from './components/Textarea.svelte'
