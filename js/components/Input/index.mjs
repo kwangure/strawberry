be80a79f1cf69e1c88f2e0768e0eef27bfbb86cc
kwangure <kaf.wangure@gmail.com>
2019-10-26 00:59:50 +0300

    fix: add base text input

--- js/components/Input/index.mjs
+++ js/components/Input/index.mjs
@@ -1,6 +1,12 @@
-import Input from './Input.svelte'
+import Input from './Input.svelte';
+import Checkbox  from './Checkbox.svelte'
+import Number from './Number.svelte'
+import Password  from './Password.svelte'
+import Radio from './Radio.svelte'
+
+Input.Checkbox = Checkbox;
+Input.Number = Number;
+Input.Password = Password;
+Input.Radio = Radio;
+
 export default Input
-export { default as Checkbox } from './Checkbox.svelte'
-export { default as Number } from './Number.svelte'
-export { default as Password } from './Password.svelte'
-export { default as Radio} from './Radio.svelte'
--- js/index.mjs
+++ js/index.mjs
@@ -1,5 +1,3 @@
-import * as Input from "./components/Input"
-
 export { default as Button } from './components/Button.svelte'
 export { default as ButtonGroup } from './components/ButtonGroup.svelte'
 export { default as ButtonTabs } from './components/ButtonTabs.svelte'
@@ -9,4 +7,4 @@ export { default as Modal } from './components/Modal.svelte'
 export { default as Sidebar } from './components/Sidebar.svelte'
 export { default as Textarea } from './components/Textarea.svelte'
 export { default as Tooltip } from './components/Tooltip.svelte'
-export { Input } ;
+export { default as Input } from './components/Input';
