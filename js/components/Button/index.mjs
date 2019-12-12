28bc30c9350c90960a6011daf9ece437c10202d4
kwangure <kaf.wangure@gmail.com>
2019-12-12 22:24:42 +0300

    refactor: move button types into 1 folder

---
+++ js/components/Button/index.mjs
@@ -0,0 +1,8 @@
+import Button from './Button.svelte';
+import Group from './Group.svelte'
+import Tabs from './Tabs.svelte'
+
+Button.Group    = Group;
+Button.Tabs     = Tabs;
+
+export default Button;
