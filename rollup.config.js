e6f48bd6137c9cec10a49eae2aa0b57a7b70ec00
kwangure <kaf.wangure@gmail.com>
2019-12-12 22:25:49 +0300

    build: compile components before exports

---
+++ rollup.config.js
@@ -0,0 +1,50 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import terser from 'rollup-plugin-terser';
+import svelte from 'rollup-plugin-svelte'
+import globals from 'rollup-plugin-node-globals'
+
+let nested  = ["Dropdown", "Input", "Sidebar", "Button"];
+let singles = ["Icon", "Modal", "Tooltip"];
+let paths = [
+    ...nested,
+    ...singles.map(component => `${component}.svelte`)
+]
+
+const production = !process.env.ROLLUP_WATCH
+
+export default [
+    ...paths.map(component => ({
+        input: `js/components/${component}`,
+        output: {
+            name: component.replace(".svelte", ""),
+            file: `dist/${component.replace(".svelte", "")}.js`,
+            format: "umd",
+            sourcemap: true,
+        },
+        plugins: [
+            svelte({
+                // enable run-time checks when not in production
+                dev: false,
+                css: true,
+                hydratable: true,
+                accessors: true,
+            }),
+            resolve({
+                browser: true,
+                dedupe: importee => importee === 'svelte' || importee.startsWith('svelte/'),
+                preferBuiltins: true
+            }),
+            commonjs(),
+            globals(),
+            production && terser(),
+        ],
+        onwarn: function (message, warn) {
+			if (message.pluginCode === 'missing-custom-element-compile-options') {
+				return
+			}
+			warn(message)
+		}
+    })),
+];
+
