434854f8d109d50ece5abc81aa710c29f5602b48
kwangure <kaf.wangure@gmail.com>
2019-12-18 19:02:18 +0300

    fix: update changed routes

--- js/index.mjs
+++ js/index.mjs
@@ -1,9 +1,7 @@
-export { default as Button } from './components/Button.svelte'
-export { default as ButtonGroup } from './components/ButtonGroup.svelte'
-export { default as ButtonTabs } from './components/ButtonTabs.svelte'
-export { default as Dropdown } from './components/Dropdown'
-export { default as Icon } from './components/Icon.svelte'
-export { default as Modal } from './components/Modal.svelte'
-export { default as Sidebar } from './components/Sidebar'
-export { default as Tooltip } from './components/Tooltip.svelte'
+export { default as Button } from './components/Button';
+export { default as Dropdown } from './components/Dropdown';
 export { default as Input } from './components/Input';
+export { default as Sidebar } from './components/Sidebar';
+export { default as Icon } from './components/Icon.svelte';
+export { default as Modal } from './components/Modal.svelte';
+export { default as Tooltip } from './components/Tooltip.svelte';
