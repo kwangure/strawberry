03ca9c0130518fb5d76f942d39ea05fb2be5792e
kwangure <kaf.wangure@gmail.com>
2019-12-18 18:55:33 +0300

    fix: make tooltip more strawberryey

--- js/components/Tooltip.svelte
+++ js/components/Tooltip.svelte
@@ -8,68 +8,66 @@
 
 <svelte:options tag="sb-tooltip"/>
 
-<span 
-    class:tooltipped={label} class:left class:center={!left && !right} 
-    class:right class:breakParent 
-    aria-label={label}>
+<span aria-label={label} class:active={label} class:left 
+    class:center={!left && !right} class:right class:breakParent>
     <slot></slot>
 </span>
 
 <style>
-    .tooltipped {
-        display: inline-block;
+    .active {
         position: relative;
     }
-    .tooltipped.breakParent {
+    .active:before,
+    .active:after {
+        opacity: 0;
+        transition: opacity 300ms ease-in-out;
+        pointer-events: none;
+        position: absolute;
+        display: block;
+        z-index: 300;
+    }
+    .active:hover:before,
+    .active:hover:after {
+        opacity: 1;
+    }
+    .active.breakParent {
         position: static;
     }
-    .tooltipped:hover:after {
+    .active:before {
+        top: calc(100%);
+        border: solid;
+        border-color: #4a4e57 transparent;
+        border-width: 0 8px 9px 8px;
+        content: "";
+    }
+    .active:after {
         line-height: 1em;
         background: #4a4e57;
         border-radius: 4px;
-        bottom: auto;
-        top: calc(100% + 7px);
+        top: calc(100% + 8px);
         color: #fff;
         content: attr(aria-label);
-        display: block;
-        padding: 7px 10px;
-        position: absolute;
+        padding: 10px;
         white-space: nowrap;
-        z-index: 300;
         font-size: 12px;
         text-transform: none;
         font-weight: 400;
-        pointer-events: none;
     }
-    .tooltipped.left:hover:after {
-        left: 0%;
+    .active.left:before {
+        left: 0px;
     }
-    .tooltipped.center:hover:after {
+    .active.left:after {
+        left: -10px;
+    }
+    .active.center:before,
+    .active.center:after {
         left: 50%;
         transform: translateX(-50%);
     }
-    .tooltipped.right:hover:after {
-        right: 0%;
+    .active.right:before {
+        right: 0px;
     }
-    .tooltipped:hover:before {
-        border: solid;
-        border-color: #4a4e57 transparent;
-        bottom: auto;
-        top: calc(100% + 2px);
-        border-width: 0 5px 5px 5px;
-        content: "";
-        display: block;
-        position: absolute;
-        z-index: 301;
-    }
-    .tooltipped.left:hover:before {
-        left: 10%;
-    }
-    .tooltipped.center:hover:before {
-        left: 50%;
-        transform: translateX(-50%);
-    }
-    .tooltipped.right:hover:before {
-        right: 10%;
+    .active.right:after {
+        right: -10px;
     }
 </style>
