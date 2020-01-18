9b6a1b0d7ef4583cb8ddaa698091cfc862355325
kafungo <kaf.wangure@gmail.com>
2020-01-18 21:10:52 +0300

    style: remove alignment spacing

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
-export { default as Button }   from './dist/Button';
+export { default as Button } from './dist/Button';
 export { default as Dropdown } from './dist/Dropdown';
-export { default as Icon }     from './dist/Icon';
-export { default as Input }    from './dist/Input';
-export { default as Modal }    from './dist/Modal';
-export { default as Sidebar }  from './dist/Sidebar';
-export { default as Tooltip }  from './dist/Tooltip';
+export { default as Icon } from './dist/Icon';
+export { default as Input } from './dist/Input';
+export { default as Modal } from './dist/Modal';
+export { default as Sidebar } from './dist/Sidebar';
+export { default as Tooltip } from './dist/Tooltip';
