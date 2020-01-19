e5cde36dc6d49d42b4a7310fdd584c309523e2ce
kafungo <kaf.wangure@gmail.com>
2020-01-20 00:52:18 +0300

    build: seive out css vars

--- strawberry.config.js
+++ strawberry.config.js
@@ -2,6 +2,7 @@ const sveltePreprocess = require("svelte-preprocess");
 const cssImport = require("postcss-import");
 const autoprefixer = require("autoprefixer");
 const cssnano = require("cssnano");
+const cssVars = require("postcss-css-variables");
 
 module.exports = {
     preprocess: sveltePreprocess({
@@ -12,6 +13,7 @@ module.exports = {
                 cssnano({
                     preset: 'default',
                 }),
+                cssVars(),
             ],
         },
     }),
