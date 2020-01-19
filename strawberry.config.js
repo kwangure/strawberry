88204d19210695336e3f1322df1fbc0e999f16a9
kafungo <kaf.wangure@gmail.com>
2020-01-19 22:44:57 +0300

    build: compress css with css nano

--- strawberry.config.js
+++ strawberry.config.js
@@ -1,13 +1,17 @@
 const sveltePreprocess = require("svelte-preprocess");
 const cssImport = require("postcss-import");
 const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
 
 module.exports = {
     preprocess: sveltePreprocess({
         postcss: {
             plugins: [
-                autoprefixer(),
                 cssImport(),
+                autoprefixer(),
+                cssnano({
+                    preset: 'default',
+                }),
             ],
         },
     }),
