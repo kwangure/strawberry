a98742b4c1b9bf9b59a5ddedf6f8050d9210847a
kafungo <kaf.wangure@gmail.com>
2020-02-11 18:33:24 +0300

    feat: add path component to icon

---
+++ js/components/Icon/Path.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let d;
+    export let fill="currentColor";
+    export let props = {};
+</script>
+
+<svelte:options tag="berry-icon-path"/>
+
+<path {d} {fill} {...props}/>
--- js/components/Icon/index.js
+++ js/components/Icon/index.js
@@ -1,3 +1,6 @@
 import Icon from "./Icon.svelte";
+import Path from "./Path.svelte";
+
+Icon.Path = Path;
 
 export default Icon;
