9813ff07d49686665ac465cc9b86af6565b90601
kwangure <kaf.wangure@gmail.com>
2020-06-26 00:32:19 +0300

    fix: compact CSS in while preprocessing

--- config/index.js
+++ config/index.js
@@ -1,32 +1,17 @@
+/* global require, module */
 const sveltePreprocess = require("svelte-preprocess");
 const cssImport = require("postcss-import");
-const autoprefixer = require("autoprefixer");
 const cssnano = require("cssnano");
 const cssVars = require("postcss-css-variables");
 
-// for use with in Svelte projects
 module.exports.preprocessConfig = sveltePreprocess({
     postcss: {
         plugins: [
             cssImport(),
-            autoprefixer(),
-            cssnano({
-                preset: 'default',
-            }),
-        ],
-    },
-});
-
-// custom elements config
-module.exports.customElementsPreprocess = sveltePreprocess({
-    postcss: {
-        plugins: [
-            cssImport(),
-            autoprefixer(),
-            cssnano({
-                preset: 'default',
-            }),
             cssVars(),
+            cssnano({
+                preset: "default",
+            }),
         ],
     },
 });
