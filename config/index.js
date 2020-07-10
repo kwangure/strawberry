4b1121cd0203b70baf8e2ca223513b0e8df4065e
kwangure <kaf.wangure@gmail.com>
2020-07-10 22:08:36 +0300

    fix: stop inlining CSS vars, instead add fallback

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 /* global require, module */
 const sveltePreprocess = require("svelte-preprocess");
 const cssnano = require("cssnano");
-const cssVars = require("postcss-css-variables");
+const cssVars = require("postcss-custom-properties");
 
 module.exports.preprocessConfig = sveltePreprocess({
     postcss: {
