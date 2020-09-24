f14ea42d6d905d5616bbe94373b5f5495b0163c7
kwangure <kaf.wangure@gmail.com>
2020-09-24 17:54:28 +0300

    refactor: use Input component as Select display

--- components/Select/Select.svelte
+++ components/Select/Select.svelte
@@ -4,6 +4,7 @@
     import { mdiChevronDown } from "@mdi/js";
     import Icon from "../Icon";
     import Dropdown from "../Dropdown";
+    import Input from "../Input";
 
     export let placeholder = "";
     export let placement = "bottomLeft";
@@ -28,68 +29,25 @@
 </script>
 
 <div class="select" >
-    {#if typeof label === "string"}
-        <div class="label">
-            {label}
-        </div>
-    {/if}
     <Dropdown {placement}>
-        <div slot="button" let:visible class:open={visible} class:disabled>
-            <input bind:value={displayText} type="text" readonly>
-            <div class="postfix" class:open={visible}>
-                <Icon path={mdiChevronDown}></Icon>
-            </div>
+        <div slot="button" class:disabled>
+            <Input bind:value={displayText} readonly {label}>
+                <div slot="postfix">
+                    <Icon path={mdiChevronDown}></Icon>
+                </div>
+            </Input>
         </div>
         <slot></slot>
     </Dropdown>
 </div>
 
 <style>
-    .select {
-        --br-postfix-width: 31px;
-        --br-postfix-height: --br-postfix-width;
-    }
-    .label {
-        margin-bottom: 5px;
-        line-height: 1.5;
-    }
-    [slot="button"] {
-        display: flex;
-        border-radius: 4px;
-        overflow: hidden;
-        border: 1px solid #d9d9d9;
-        padding: 1px;
-    }
-    [slot="button"]:hover,
-    [slot="button"].open {
-        border: 2px solid #196eff;
-        outline: none;
-        padding: 0px;
-    }
-    [slot="button"]:hover .postfix, 
-    .postfix.open {
-        color: var(--br-primary);
-        background-color: var(--br-primary-light);
+    [slot="postfix"] {
+        all: inherit;
     }
     [slot="button"].disabled {
         pointer-events: none;
         cursor: not-allowed;
-    }
-    input {
-        border: none;
-        outline: none;
-        height: 32px;
-        user-select: none;
-        flex: 1;
-        user-select: none;
-        padding: var(--br-input-vertical-padding) var(--br-input-horizontal-padding);
-    }
-    .postfix {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        width: var(--br-postfix-width);
-        height: var(--br-postfix-height);
     }
 </style>
 
