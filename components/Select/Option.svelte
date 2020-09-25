e2d1f6cb3e4fcef657403e3b24d6e5c39bdf075c
kwangure <kaf.wangure@gmail.com>
2020-09-25 20:50:34 +0300

    feat: handle options like native select

--- components/Select/Option.svelte
+++ components/Select/Option.svelte
@@ -1,27 +1,30 @@
 <script>
-    import { SELECT_VALUE_STORE_NAME } from "./Select.svelte";
+    import { ACTIVE_OPTION, OPTIONS_STORE, UPDATE_VALUE_FUNC } from "./Select.svelte";
     import { getContext } from "svelte";
+    import uid from "uid";
 
     export let value;
-    const selectValueStore = getContext(SELECT_VALUE_STORE_NAME);
 
-    let optionDisplayText = "";
-    function getDisplayText(divElement) {
-        optionDisplayText = divElement.innerText;
-        
-        // Set display text of <Select/> if this <Option/> is initially selected
-        if ($selectValueStore.value === value) {
-            setParentSelectValue();
+    const id = uid();
+    const activeOptionId = getContext(ACTIVE_OPTION);
+    const options = getContext(OPTIONS_STORE);
+    const updateSelectValueFn = getContext(UPDATE_VALUE_FUNC);
+    let option = null;
+    let optionValue = {};
+
+    $: changeOptionValue(option, value);
+
+    function changeOptionValue(option, value){
+        if (option) {
+            optionValue = { displayText: option.innerText, id, value };
+            $options = $options.set(id, optionValue);
         }
     }
-
-    function setParentSelectValue() {
-        $selectValueStore = { displayText: optionDisplayText, value };
-    }      
 </script>
 
-<div class="berry-option menu-item" class:active={$selectValueStore.value === value}
-    on:mousedown={setParentSelectValue} use:getDisplayText>
+<div bind:this={option} class="berry-option menu-item" 
+    class:active={id === $activeOptionId} 
+    on:mousedown={() => updateSelectValueFn(optionValue)}>
     <slot></slot>
 </div>
 
--- components/Select/Select.svelte
+++ components/Select/Select.svelte
@@ -1,7 +1,7 @@
 <script>
     import { createEventDispatcher, setContext } from "svelte";
-    import { writable } from "svelte/store";
     import { mdiChevronDown } from "@mdi/js";
+    import { writable } from "svelte/store";
     import Icon from "../Icon";
     import Dropdown from "../Dropdown";
     import Input from "../Input";
@@ -11,21 +11,47 @@
     export let value = "";
     export let label = "";
     export let disabled = false;
-    let displayText = placeholder;
 
-    const selectValueStore = writable({ displayText, value });
     const dispatch = createEventDispatcher();
+    const activeOptionId = writable("");
+    const options = writable(new Map());
+    let valueIsDirty = false;
+    let displayText = "";
 
-    $: handleValueChange($selectValueStore);
+    $: handleValueChange($options, value);
     
-    function handleValueChange(selectValue) {
-        ({ displayText, value } = selectValue);
-        dispatch("change", value);
+    function handleValueChange(options, value) {
+        // value was changed by `updateSelectValue`
+        if (valueIsDirty) {
+            valueIsDirty = false;
+            dispatch("change", value);
+            return;
+        }
+
+        // value was changed via props
+        for (const [_id, option] of options) {
+            if (option.value === value) {
+                ({ displayText, id: $activeOptionId } = option);
+                return;
+            }
+        }
+
+        // value doesn't match any child <Option/>
+        displayText = placeholder;
+        if (import.meta.env.DEV && options.size > 0) {
+            console.error(`No option with the value "${value}" was found.`);
+        }
     }
-    
-    // Children of <Select/> (i.e <Option/>) change the value of 
-    // this parent <Select/> via `getContext`
-    setContext(SELECT_VALUE_STORE_NAME, selectValueStore);
+
+    function updateSelectValue(option) {
+        // update scoped vars from outside
+        ({ value, displayText, id :$activeOptionId } = option);
+        valueIsDirty = true;
+    }
+
+    setContext(ACTIVE_OPTION, activeOptionId);
+    setContext(OPTIONS_STORE, options);
+    setContext(UPDATE_VALUE_FUNC, updateSelectValue);
 </script>
 
 <div class="select" >
@@ -52,5 +78,7 @@
 </style>
 
 <script context="module">
-    export const SELECT_VALUE_STORE_NAME = "custom-select-option";
+    export const ACTIVE_OPTION = "select-active-option";
+    export const OPTIONS_STORE = "select-options-map";
+    export const UPDATE_VALUE_FUNC = "select-update-func";
 </script>
