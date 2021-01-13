03c8edc5b278a250b87a58ea69e826b1e9a6ed0d
kwangure <kaf.wangure@gmail.com>
2021-01-13 18:39:35 +0300

    feat!: pass props to `Path` without needing a `props` object.
    BREAKING CHANGE: pass props normally to path (i.e now use
    `<Path stroke="red"/>` instead of `<Path props={stroke:"red"}/>`

--- components/Icon/Path.svelte
+++ components/Icon/Path.svelte
@@ -1,7 +1,6 @@
 <script>
     export let d;
     export let fill="currentColor";
-    export let props = {};
 </script>
 
-<path class="berry-icon-path" {d} {fill} {...props}/>
+<path class="berry-icon-path" {d} {fill} {...$$restProps}/>
