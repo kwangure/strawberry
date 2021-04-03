55bada64c7d77b4ceb7e8c38ef750b6e79488ccd
kwangure <kaf.wangure@gmail.com>
2021-04-03 20:52:21 +0300

    refactor: don't directly import from component

--- site/routes/modal.svelte
+++ site/routes/modal.svelte
@@ -1,5 +1,5 @@
 <script>
-    import Button from "~@components/Button/Button.svelte";
+    import Button from "~@components/Button/";
     import Modal from "~@components/Modal";
 
     let visible = false;
