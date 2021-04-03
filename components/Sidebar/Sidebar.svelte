0e12287b66f90a545d5feec3d9e9a884512dfced
kwangure <kaf.wangure@gmail.com>
2021-04-03 19:28:29 +0300

    feat!: remove head and footer `Sidebar` API

--- components/Sidebar/Sidebar.svelte
+++ components/Sidebar/Sidebar.svelte
@@ -1,48 +1,14 @@
-<script>
-    import Item from "./Item.svelte";
-    export let items = [];
-</script>
-
 <div class="berry-sidebar">
-    {#if $$slots.head}
-        <div class="sidebar-head">
-            <slot name="head"></slot>
-        </div>
-    {/if}
-    <div class="sidebar-items">
-        {#each items as item, index}
-            <Item on:click={item.onclick} active={item.active}>
-                <slot name="item" item={{ ...item, index }}></slot>
-            </Item>
-        {:else}
-            <slot/>
-        {/each}
-    </div>
-    {#if $$slots.foot}
-        <div class="sidebar-foot">
-            <slot name="foot"></slot>
-        </div>
-    {/if}
+    <slot/>
 </div>
 
 <style>
     .berry-sidebar {
         width: 240px;
-        padding: 0 10px;
+        padding: var(--br-outer-gutter);
         height: 100%;
         display: flex;
         flex-direction: column;
-        flex: none;
         user-select: none;
-    }
-    .sidebar-foot {
-        margin-top: auto;
-    }
-    .sidebar-head{
-        display: block;
-        min-height: 40px;
-        line-height: 24px;
-        text-decoration: none;
-        padding: 8px 12px;
     }
 </style>
