56f810fe842f2c1a0aabb0d939d889d4c1b54d31
kwangure <kaf.wangure@gmail.com>
2021-04-04 04:14:53 +0300

    fix: use proper label API

--- site/routes/select.svelte
+++ site/routes/select.svelte
@@ -2,6 +2,7 @@
     import Select, { Option } from "~@components/Select";
 </script>
 
-<Select placeholder="Like I said..." label="Pick me! I'm smart.">
+<Select placeholder="Like I said...">
+    <svelte:fragment slot="label">Pick me! I'm smart.</svelte:fragment>
     <Option value={"testicle"}>Testicle Rainbow</Option>
 </Select>
