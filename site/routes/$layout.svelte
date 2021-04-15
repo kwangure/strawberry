32db01530f17c73c3b3c6133954eb339ef9303cf
kwangure <kaf.wangure@gmail.com>
2021-04-15 14:43:10 +0300

    site: cleanup sidebar

--- site/routes/$layout.svelte
+++ site/routes/$layout.svelte
@@ -1,9 +1,9 @@
 <script>
     import "~@css/standardDOM";
     import Sidebar, { Link, Section } from "~@components/Sidebar";
+    import { base } from "$app/paths";
     import Nav from "./_$layout.svelte/Nav.svelte";
     import { page } from "$app/stores";
-    import { base } from "$app/paths";
 
     $: ({ path } = $page);
 </script>
@@ -13,17 +13,25 @@
     <div class="components">
         <div class="left">
             <Sidebar>
-                <Link href="{base}/button" {path}>
-                    Button
-                </Link>
-                <Link href="{base}/dropdown" {path}>
-                    Dropdown
-                </Link>
-                <Link href="{base}/icon" {path}>
-                    Icon
+                <Link href="{base}/" {path}>
+                    Components Overview
                 </Link>
                 <Section>
-                    <svelte:fragment slot="title">Inputs</svelte:fragment>
+                    <svelte:fragment slot="title">General</svelte:fragment>
+                    <svelte:fragment slot="items">
+                        <Link href="{base}/button" {path}>
+                            Button
+                        </Link>
+                        <Link href="{base}/dropdown" {path}>
+                            Dropdown
+                        </Link>
+                        <Link href="{base}/icon" {path}>
+                            Icon
+                        </Link>
+                    </svelte:fragment>
+                </Section>
+                <Section>
+                    <svelte:fragment slot="title">Data Inputs</svelte:fragment>
                     <svelte:fragment slot="items">
                         <Link href="{base}/input" {path}>
                             Text
@@ -37,23 +45,28 @@
                         <Link href="{base}/input/radio" {path}>
                             Radio
                         </Link>
+                        <Link href="{base}/select" {path}>
+                            Select
+                        </Link>
                     </svelte:fragment>
                 </Section>
-                <Link href="{base}/list" {path}>
-                    List
-                </Link>
-                <Link href="{base}/modal" {path}>
-                    Modal
-                </Link>
-                <Link href="{base}/notification" {path}>
-                    Notification
-                </Link>
-                <Link href="{base}/select" {path}>
-                    Select
-                </Link>
-                <Link href="{base}/tooltip" {path}>
-                    Tooltip
-                </Link>
+                <Section>
+                    <svelte:fragment slot="title">Data Display</svelte:fragment>
+                    <svelte:fragment slot="items">
+                        <Link href="{base}/list" {path}>
+                            List
+                        </Link>
+                        <Link href="{base}/modal" {path}>
+                            Modal
+                        </Link>
+                        <Link href="{base}/notification" {path}>
+                            Notification
+                        </Link>
+                        <Link href="{base}/tooltip" {path}>
+                            Tooltip
+                        </Link>
+                    </svelte:fragment>
+                </Section>
             </Sidebar>
         </div>
         <div class="right">
