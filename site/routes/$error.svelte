4ac61de8ec94406a351b1b574816c5a477db8a86
kwangure <kaf.wangure@gmail.com>
2021-05-06 11:58:47 +0300

    fix: guard against missing error

--- site/routes/$error.svelte
+++ site/routes/$error.svelte
@@ -5,6 +5,24 @@
 	export let error;
 </script>
 
+<svelte:head>
+	<title>{status}</title>
+</svelte:head>
+
+<h1>{status}</h1>
+
+{#if error?.message}
+	<p>{error.message}</p>
+{:else}
+	<p>Unknown error: {status}</p>
+{/if}
+
+{#if error?.stack}
+	<pre>{error.stack}</pre>
+{:else}
+	<pre>Unknown Error Source</pre>
+{/if}
+
 <style>
 	h1, p {
 		text-align: center;
@@ -14,16 +32,3 @@
 		margin-top: 2em;
 	}
 </style>
-
-<svelte:head>
-	<title>{status}</title>
-</svelte:head>
-
-<h1>{status}</h1>
-
-<p>{error.message}</p>
-
-{#if error.stack}
-	<pre>{error.stack}</pre>
-{/if}
-
