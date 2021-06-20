a5d88333b2d2b2ef4db73bd55254a7bdb3a8f373
kwangure <kaf.wangure@gmail.com>
2021-06-21 00:40:29 +0300

    fix: update eslint to latest svelte-kit API

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,7 +10,7 @@ module.exports = {
                     ["~@components", "./components/"],
                     ["~@css", "./css/"],
                     // escape `$` to work around eslint's Regex matching
-                    ["\\$app", "./.svelte/dev/runtime/app/"],
+                    ["\\$app", "./.svelte-kit/dev/runtime/app/"],
                     ["\\$lib", "src/lib/"],
                 ],
                 extensions: [".js", ".svelte", ".json"],
