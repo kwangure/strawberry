5c0ca8a2774d69f5fd4232318d8d33e8d5236d87
kwangure <kaf.wangure@gmail.com>
2021-06-25 23:13:23 +0300

    docs(site): document button props

---
+++ src/lib/components/component.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let name;
+</script>
+
+<svelte:head>
+    <!-- Component title for SEO -->
+    <title> Svelte {name} Component </title>
+</svelte:head>
+
+<h1>{name}</h1>
+<h3>Props</h3>
+<slot name="props"/>
+
+<style>
+    h1 {
+        font-size: 32px;
+    }
+    h3 {
+        font-weight: 400;
+        margin: 16px 0 8px;
+    }
+</style>
---
+++ src/lib/components/prop.svelte
@@ -0,0 +1,52 @@
+<script>
+    import Code from "~@components/Code";
+
+    export let name;
+    export let type;
+    let defaultValue = "";
+    export { defaultValue as default };
+</script>
+
+<details class="prop">
+    <summary>
+        <span>{name}</span>
+        <p><slot/></p>
+    </summary>
+
+    <Code language="javascript">
+{#if defaultValue}
+/**
+ * @type {type}
+ * @default {defaultValue}
+ */
+{:else}
+/**
+ * @type {type}
+ */
+{/if}
+let {name};
+    </Code>
+
+    {#if $$slots.usage}
+        <p>Usage:</p>
+        <Code language="svelte">
+            <slot name="usage"/>
+        </Code>
+    {/if}
+
+    {#if $$slots.example}
+        <p>Example:</p>
+        <div class="example">
+            <slot name="example"/>
+        </div>
+    {/if}
+</details>
+
+<style>
+    .prop {
+        margin-bottom: 10px;
+    }
+    p {
+        margin: 1em 0 0.25em;
+    }
+</style>
