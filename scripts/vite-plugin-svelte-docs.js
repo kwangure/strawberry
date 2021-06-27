35e1f9900332ea5b9ffdbd74316ee3862784e022
kwangure <kaf.wangure@gmail.com>
2021-06-27 21:23:24 +0300

    chore(docs): add docs vite plugin

---
+++ scripts/vite-plugin-svelte-docs.js
@@ -0,0 +1,81 @@
+import escapeStringRegexp from "escape-string-regexp";
+import { readFileSync } from "fs";
+import sveltedoc from "sveltedoc-parser";
+
+export default function docs(opts = {}) {
+    opts = Object.assign({}, { prefix: "docs" }, opts);
+
+    const prefix = `${opts.prefix}:`;
+    const postfix = ".svelte-doc";
+    const docFiles = new Map();
+
+    function cleanId(id) {
+        return id
+            .replace(new RegExp(`^${escapeStringRegexp(prefix)}`), "")
+            .replace(new RegExp(`${escapeStringRegexp(postfix)}$`), "");
+    }
+
+    function affixId(id) {
+        return `${prefix}${id}${postfix}`;
+    }
+
+    return {
+        name: "svelte-docs",
+        async resolveId(id, importer) {
+            if (!id.startsWith(prefix)) return;
+
+            const path = cleanId(id);
+            const resolved = await this.resolve(path, importer);
+            if (!resolved) {
+                throw Error(`Cannot find module '${path}' from '${importer}'`);
+            }
+
+            // Add custom postfix to path to avoid conflicts with Svelte plugin
+            return affixId(resolved.id);
+        },
+
+        load(id) {
+            if (!id.startsWith(prefix)) return;
+
+            return readFileSync(cleanId(id), { encoding: "utf-8" });
+        },
+
+        async transform(code, id) {
+            if (!id.startsWith(prefix)) return;
+
+            const realFilepath = cleanId(id);
+            const docs = await sveltedoc.parse({
+                filename: realFilepath,
+                fileContent: code,
+                version: 3,
+            });
+            const { description, name, data: props, slots } = docs;
+            const jsExports
+= `export default ${JSON.stringify({ description, name, props, slots })};
+export const description = "${description}";
+export const name = "${name}";
+export const props = ${JSON.stringify(props)};
+export const slots = ${JSON.stringify(slots)};`;
+
+            docFiles.set(realFilepath, jsExports);
+
+            return {
+                code: jsExports,
+                map: null,
+            };
+        },
+
+        // TODO: Compare compile results to only reload when docs have changed
+        handleHotUpdate(hmrContext) {
+            const { file, server } = hmrContext;
+            const isDocFile = docFiles.has(file);
+
+            if (!isDocFile) return;
+
+            const docModule = server.moduleGraph.getModuleById(affixId(file));
+
+            return [docModule];
+        },
+    };
+
+}
