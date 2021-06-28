3ff2daf289dd670681623f515a291abced6b09ff
kwangure <kaf.wangure@gmail.com>
2021-06-28 03:43:24 +0300

    Updates

---
+++ app/pages/components/list.svelte-bd6b29c6.js
@@ -0,0 +1,1 @@
+import{S as s,i as t,s as a,t as r,g as e,f as n,H as o,d as u}from"../../chunks/vendor-352f3030.js";function c(s){let t;return{c(){t=r("// TODO")},l(s){t=e(s,"// TODO")},m(s,a){n(s,t,a)},p:o,i:o,o:o,d(s){s&&u(t)}}}export default class extends s{constructor(s){super(),t(this,s,null,c,a,{})}}
