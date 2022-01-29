6b3e146151bdd13811d7534a128ba29ba23d6b48
kwangure <kaf.wangure@gmail.com>
2022-01-29 21:25:20 +0300

    Updates

---
+++ app/chunks/forward-events-e4dd3255.js
@@ -0,0 +1,1 @@
+import{_ as c,$ as a,a0 as l}from"./vendor-d49c0083.js";function p(){const r=l();return t=>{const n=[];for(const e in r.$$.callbacks)if({}.hasOwnProperty.call(r.$$.callbacks,e)){const o=c(t,e,s=>{a(r,s)});n.push(o)}return{destroy:()=>{n.forEach(e=>e())}}}}export{p as c};
