1f1906a3709e9c5c748b0d5f2173ba2e4a58b424
kwangure <kaf.wangure@gmail.com>
2022-05-06 21:01:48 +0300

    chore: terminate stale installs early

--- scripts/local-pkg-watch/install.js
+++ scripts/local-pkg-watch/install.js
@@ -1,13 +1,35 @@
-import { exec as _exec } from "child_process";
-import util from "util";
+import { spawn as _spawn } from "child_process";
 
-const exec = util.promisify(_exec);
+const installing = new Map();
+export default function install(pkg, dev) {
+    if (installing.has(pkg)) {
+        installing.get(pkg).kill("SIGINT");
+    }
+    console.log("starting install");
 
-export default async function install(pkg, dev) {
     const flags = dev ? "-D" : "";
-    const installCmd = `npm install ${flags} ${pkg}`;
-    console.log(installCmd);
-    const { stdout, stderr } = await exec(installCmd);
-    console.log(stdout);
-    console.log(stderr);
+    const installProcess = _spawn("npm", ["install", flags, pkg]);
+    installing.set(pkg, installProcess);
+
+    installProcess.stdout.on("data", (output) => {
+        console.log(output.toString());
+    });
+
+    installProcess.stderr.on("data", (output) => {
+        console.error(output.toString());
+    });
+
+    return new Promise((resolve) => {
+        installProcess.on("exit", (code) => {
+            installing.delete(pkg);
+            console.log(`Child exited with code ${code}`);
+            resolve(code);
+        });
+        installProcess.on("error", (error) => {
+            installing.get(pkg).kill("SIGINT");
+            installing.delete(pkg);
+            console.error(error);
+            resolve(error.code ?? 1);
+        });
+    });
 }
