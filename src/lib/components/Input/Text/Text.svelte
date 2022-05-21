ee4f11bb968842ba69c7da1e5b07e924a9cb6f33
kwangure <kaf.wangure@gmail.com>
2022-05-21 10:45:38 +0300

    feat!: refine input validation

--- src/lib/components/Input/Text/Text.svelte
+++ src/lib/components/Input/Text/Text.svelte
@@ -2,6 +2,7 @@
     import Container from "../Container.svelte";
     import { createEventForwarder } from "../../../utils/forward-events.js";
     import { focusElement } from "../actions";
+    import { listen } from "svelte/internal";
     import { slide } from "svelte/transition";
 
     /**
@@ -22,27 +23,150 @@
      */
     export let focus = false;
     /**
-     * A function to check whether the "value" is valid.
-     * @type {(value: string) => boolean}
+     * A function that returns the validity of the input.
+     * @type {((input: InputInfo) => string | Promise<string>) | undefined}
      */
-    export let invalid = (value) => false; // eslint-disable-line no-unused-vars
+    export let invalid = undefined;
+    /**
+     * A function that returns the validity of the input.
+     * @type {((error: string, input: InputInfo) => string | Promise<string>) | undefined}
+     */
+    export let error = undefined;
 
     const forward = createEventForwarder();
 
-    function handleBlur() {
-        isInvalid = invalid(value);
+    /**
+     * @typedef {{
+     *     max: string;
+     *     min: string;
+     *     maxLength: number;
+     *     minLength: number;
+     *     type: string;
+     *     value: string;
+     * }} InputInfo
+     * @typedef {{
+    *     invalid?: (input: InputInfo) => string  | Promise<string>,
+    *     error?: (error: string, input: InputInfo) => string | Promise<string>,
+    * }} ValidateOptions
+    * @param {HTMLInputElement} input
+    * @param {ValidateOptions} options
+    */
+    function validate(input, options) {
+        const noop = () => "";
+        let { invalid = noop, error = noop } = options;
+        /**
+         * @param {HTMLInputElement} input
+         */
+        async function _validate(input) {
+            if (input.disabled) return "";
+            input.dataset.invalid = "false";
+            for (const validityCheck in input.validity) {
+                // 'valid' is the only non-error boolean on HTMLInputElement ValidityState
+                if (validityCheck === "valid") continue;
+
+                // @ts-ignore
+                if (input.validity[validityCheck]) {
+                    return _error(validityCheck)
+                }
+            }
+
+            const customValidityCheck = await invalid(_input(input));
+            return (customValidityCheck) ? _error(customValidityCheck) : "";
+        }
+
+        /**
+        * @param {string} invalidType
+        */
+        async function _error(invalidType) {
+            const errorString = await error(invalidType, _input(input));
+            if (errorString) return errorString;
+
+            switch (invalidType) {
+                case "valueMissing":
+                    return "Please fill out this field.";
+                case "typeMismatch":
+                    if (input.type === "email") {
+                        return "Please enter an email address.";
+                    }
+                    if (input.type === "url") {
+                        return "Please enter a URL.";
+                    }
+                case "tooShort":
+                    return `Input length should be ${input.minLength} characters or more. You are currently using ${input.value.length} characters.`;
+                case "tooLong":
+                    return `Input length should be less than ${input.maxLength} characters. You are currently using ${input.value.length} characters.`;
+                case "badInput":
+                    return "Please enter a number.";
+                case "stepMismatch":
+                    return "Please select a valid value.";
+                case "rangeOverflow":
+                    return `Input value should not be more than ${input.max}.`;
+                case "rangeUnderflow":
+                    return `Input value should not be less than ${input.min}.`;
+                case "patternMismatch":
+                    return "Please match the requested format.";
+
+            }
+
+            // If all else fails, return a generic catchall error
+            return "The value you entered for this field is invalid.";
+        }
+
+        /**
+        * @param {HTMLInputElement} input
+        */
+        function _input(input) {
+            return {
+                max: input.max,
+                min: input.min,
+                maxLength: input.maxLength,
+                minLength: input.minLength,
+                type: input.type,
+                value: input.value,
+            };
+        }
+
+        // Show error onblur. Hide error onblur and oninput.
+        /**
+        * @type {() => void}
+        */
+        let unlisten2;
+        const unlisten = listen(input, "blur", async () => {
+            isInvalid = await _validate(input);
+            input.dataset.invalid = String(!!isInvalid);
+            if (isInvalid) {
+                unlisten2 = listen(input, "input", async () => {
+                    isInvalid = await _validate(input);
+                    if (!isInvalid) unlisten2();
+                });
+            }
+        });
+
+        return {
+            /**
+            * @param {ValidateOptions} options
+            */
+            update(options) {
+                ({ invalid = noop, error = noop } = options);
+            },
+            destroy() {
+                unlisten();
+                unlisten2?.();
+            },
+        };
     }
 
-    let isInvalid = false;
+    /**
+    * @type {string}
+    */
+    let isInvalid = "";
 </script>
 
 <Container {hideLabel} let:labelId>
     <slot name="label" slot="label"/>
     <div class="container">
-        <input class="text-input" bind:value class:isInvalid
-            on:blur={handleBlur} use:forward
-            on:keydown
-            type="text" id={labelId} use:focusElement={focus} {...$$restProps}>
+        <input class="text-input" bind:value use:forward use:validate={{ invalid, error }}
+            on:keydown type="text" id={labelId} use:focusElement={focus} {...$$restProps}>
         {#if $$slots.postfix}
             <div class="postfix-wrapper">
                 <slot name="postfix"/>
