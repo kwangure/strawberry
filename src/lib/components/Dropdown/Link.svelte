9da1a22fb2c47160162eefd66052fa0ca9b00e2d
kwangure <kaf.wangure@gmail.com>
2022-06-06 13:07:24 +0300

    feat!: add active and download attribute

--- src/lib/components/Dropdown/Link.svelte
+++ src/lib/components/Dropdown/Link.svelte
@@ -4,12 +4,26 @@
     export let path = "";
     export let href = "";
 
+    $: if (path) {
+        console.error("The 'path' attribute has been depracated. Use the 'active' prop to mark link active.")
+    }
+
+    /**
+     * @type {boolean}
+     */
+    export let active = false;
+
+    /**
+     * @type {string | boolean | undefined}
+     */
+    export let download = undefined;
+
     $: active = href === path;
 
     const forward = createEventForwarder();
 </script>
 
-<a {href} class="menu-item" use:forward class:active>
+<a {href} class="menu-item" {download} use:forward class:active>
     <slot/>
 </a>
 
