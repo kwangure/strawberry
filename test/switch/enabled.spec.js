f6b9b14f05a36eee4bce85d697df34580bae65cd
kwangure <kaf.wangure@gmail.com>
2022-06-27 10:16:39 +0300

    chore(test): fix enabled test

--- test/switch/enabled.spec.js
+++ test/switch/enabled.spec.js
@@ -1,10 +1,10 @@
 import { describe, it } from "vitest"
 import { page, queries } from "../../src/lib/internal/test/playwright.js";
 
-describe("Not disabled switch", async () => {
+describe("Enabled switch", async () => {
     let switchLocator;
-    it("is not disabled when 'disabled' prop is false", async ({ expect }) => {
-        await page.goto("http://localhost:3000/test/components/switch/not_disabled");
+    it("is enabled when 'disabled' prop is false", async ({ expect }) => {
+        await page.goto("http://localhost:3000/test/components/switch/enabled");
         switchLocator = await queries.getByRole("switch");
         await expect(switchLocator).not.toBeDisabled();
     });
