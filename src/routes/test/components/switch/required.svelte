2a45dbc48fda21cb5b3e883a1304ac804400bf75
kwangure <kaf.wangure@gmail.com>
2022-06-27 10:48:25 +0300

    chore(test): Switch required

---
+++ src/routes/test/components/switch/required.svelte
@@ -0,0 +1,7 @@
+<script>
+    import Switch from "$lib/components/Switch/Switch.svelte";
+</script>
+
+<form id="foo">
+    <Switch required name="fizz" value="buzz"/>
+</form>
---
+++ test/switch/required.spec.js
@@ -0,0 +1,15 @@
+import { describe, it } from "vitest";
+import { page } from "../../src/lib/internal/test/playwright.js";
+
+describe.only("Required switch", async () => {
+    it("prevents submission when 'required' prop is true and Switch is unchecked", async ({ expect }) => {
+        await page.goto("http://localhost:3000/test/components/switch/required");
+        await Promise.all([
+            page.waitForNavigation({ timeout: 500 }),
+            page.evaluate(() => foo.submit()),
+        ]);
+        const search = await page.evaluate(() => location.search);
+        const searchParams = new URLSearchParams(search);
+        expect(searchParams.get("fizz")).toBeNull();
+    });
+});
