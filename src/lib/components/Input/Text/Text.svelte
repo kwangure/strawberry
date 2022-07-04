ef5e9905747ca3b68920ccd6f25f5c060cc362e7
kwangure <kaf.wangure@gmail.com>
2022-07-04 10:49:58 +0300

    feat!: enumerate Text component props explicitly

--- src/lib/components/Input/Text/Text.svelte
+++ src/lib/components/Input/Text/Text.svelte
@@ -6,32 +6,107 @@
     import { slide } from "svelte/transition";
 
     /**
+     * Guidance to the browser on information expected in the field.
+     * Helps UserAgent automate filling form field values.
+     *
+     * @type {"off" | "on" | string}
+     */
+    export let autocomplete = "";
+
+    /**
+     * Whether select should have input focus when the page loads.
+     *
+     * @type {boolean}
+     */
+    export let autofocus = false;
+
+    /**
+     * A function that returns the validity of the input.
+     * @type {((error: string, input: InputInfo) => string | Promise<string>) | undefined}
+     */
+    export let error = undefined;
+
+    /**
+     * The id of the form to associate with the text input
+     *
+     * @type {string | undefined}
+     */
+    export let form = undefined;
+
+    /**
      * Whether to hide the input label.
      *
      * @type {boolean}
      */
     export let hideLabel = false;
+
+    /**
+     * A function that returns the validity of the input.
+     * @type {((input: InputInfo) => string | Promise<string>) | undefined}
+     */
+    export let invalid = undefined;
+
+    /**
+     * The id of a <datalist> element located in the same document.
+     *
+     * @type {string}
+     */
+    export let list = "";
+
+    /**
+     * The maximum number of characters allowed in the element.
+     *
+     * @type {number}
+     */
+    export let maxlength = -1;
+
+    /**
+     * The minimum number of characters allowed in the element.
+     *
+     * @type {number}
+     */
+    export let minlength = -1;
+
+    /**
+     * The name of the name input. Submitted with its parent form as part of a name/value pair.
+     *
+     * @type {string}
+     */
+    export let name = "";
+
+    /**
+     * Text that appears in the form control when it has no value set
+     *
+     * @type {string}
+     */
+    export let placeholder = "";
+
+    /**
+     * When true, the user cannot edit the value of the input
+     *
+     * @type {boolean}
+     */
+    export let readonly = false;
+
+    /**
+     * When true, indicates that the user must input a value before the parent form can be submitted.
+     *
+     * @type {boolean}
+     */
+    export let required = false;
+
     /**
      * The initial value of the component.
      * @type {string | undefined}
      */
     export let value = "";
+
     /**
      * Whether the input is focused.
      *
      * @type {boolean}
      */
     export let focus = false;
-    /**
-     * A function that returns the validity of the input.
-     * @type {((input: InputInfo) => string | Promise<string>) | undefined}
-     */
-    export let invalid = undefined;
-    /**
-     * A function that returns the validity of the input.
-     * @type {((error: string, input: InputInfo) => string | Promise<string>) | undefined}
-     */
-    export let error = undefined;
 
     const forward = createEventForwarder();
 
@@ -165,8 +240,11 @@
 <Container {hideLabel} let:labelId>
     <slot name="label" slot="label"/>
     <div class="container">
+        <!-- svelte-ignore a11y-autofocus -->
         <input class="text-input" bind:value use:forward use:validate={{ invalid, error }}
-            on:keydown type="text" id={labelId} use:focusElement={focus} {...$$restProps}>
+            on:keydown type="text" id={labelId} use:focusElement={focus}
+            {autocomplete} {autofocus} {form} {list} {maxlength} {minlength}
+            {name} {placeholder} {readonly} {required}>
         {#if $$slots.postfix}
             <div class="postfix-wrapper">
                 <slot name="postfix"/>
