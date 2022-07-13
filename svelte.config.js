1118cc5f2fb9be5cef7dcda7cfabacb575f70d37
kwangure <kaf.wangure@gmail.com>
2022-07-13 22:44:15 +0300

    chore(deps): upgrade SvelteKit and Typescript

--- svelte.config.js
+++ svelte.config.js
@@ -1,35 +1,19 @@
 import adapter from "@sveltejs/adapter-static";
 import docs from "@kwangure/svelte-docs";
-import { fileURLToPath } from "url";
 import inlineImport from "./scripts/preprocess-css-inline-import.js";
-import inspect from "vite-plugin-inspect";
-import localPackageWatch from "./scripts/local-pkg-watch/index.js";
 import micromatch from 'micromatch';
-import path from "path";
-import { strawberry } from "./src/lib/build/vite-plugin-strawberry.js";
-import { sveltekitPluginDtl } from "sitgent/plugin";
 
 const MODE = process.env.NODE_ENV;
 const PROD = MODE === "production";
 
-export const __filename = fileURLToPath(import.meta.url);
-export const __dirname = path.dirname(__filename);
-
-function resolve(pathname) {
-    return path.resolve(__dirname, pathname);
-}
-
-const appDir = "app";
-const preprocess = [
-    docs(),
-    inlineImport,
-];
-
 /** @type {import("@sveltejs/kit").Config} */
 export default {
-    preprocess,
+    preprocess: [
+        docs(),
+        inlineImport,
+    ],
     kit: {
-        appDir,
+        appDir: "app",
         adapter: adapter(),
         package: {
             exports: (filepath) => {
@@ -61,24 +45,6 @@ export default {
         },
         paths: {
             base: PROD ? "/strawberry" : "",
-        },
-        vite: {
-            plugins: [
-                inspect(),
-                strawberry({
-                    appDir,
-                }),
-                localPackageWatch(),
-                sveltekitPluginDtl(),
-            ],
-            resolve: {
-                alias: {
-                    "~@css": resolve("./src/lib/css/"),
-                    "~@components": resolve("./src/lib/components/"),
-                    "~@utils": resolve("./src/lib/utils/"),
-                    "@kwangure/strawberry/css/styles": resolve("./src/lib/css/styles.js"),
-                },
-            },
         },
     },
 };
