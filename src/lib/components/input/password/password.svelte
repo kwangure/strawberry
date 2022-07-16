421a6d742e1a475d5558fd7d01e19f07924efb52
kwangure <kaf.wangure@gmail.com>
2022-07-16 16:01:33 +0300

    feat!: remove restProps from password input

--- src/lib/components/input/password/password.svelte
+++ src/lib/components/input/password/password.svelte
@@ -9,6 +9,21 @@
     import { writable } from 'svelte/store';
 
     /**
+     * Guidance to the browser on information expected in the field.
+     * Helps UserAgent automate filling form field values.
+     *
+     * @type {"off" | "on" | string}
+     */
+    export let autocomplete = '';
+
+    /**
+     * When true, the password input will have input focus when the page loads.
+     *
+     * @type {boolean}
+     */
+    export let autofocus = false;
+
+    /**
      * A function that takes a validity state string and returns an error message.
      *
      * @type {((error: string, input: HTMLInputElement) => string) | undefined}
@@ -16,11 +31,67 @@
     export let error = undefined;
 
     /**
+     * The id of the form to associate with the text input
+     *
+     * @type {string | undefined}
+     */
+    export let form = undefined;
+
+    /**
      * A function that returns the validity of the input.
      *
      * @type {((input: HTMLInputElement) => string) | undefined}
      */
     export let invalid = undefined;
+
+    /**
+     * The id of a <datalist> element located in the same document.
+     *
+     * @type {string}
+     */
+    export let list = '';
+
+    /**
+     * The maximum number of characters allowed in the element.
+     *
+     * @type {number}
+     */
+    export let maxlength = -1;
+
+    /**
+     * The minimum number of characters allowed in the element.
+     *
+     * @type {number}
+     */
+    export let minlength = -1;
+
+    /**
+     * The name of the name input. Submitted with its parent form as part of a name/value pair.
+     *
+     * @type {string}
+     */
+    export let name = '';
+
+    /**
+     * Text that appears in the form control when it has no value set
+     *
+     * @type {string}
+     */
+    export let placeholder = '';
+
+    /**
+     * When true, the user cannot edit the value of the input
+     *
+     * @type {boolean}
+     */
+    export let readonly = false;
+
+    /**
+     * When true, indicates that the user must input a value before the parent form can be submitted.
+     *
+     * @type {boolean}
+     */
+    export let required = false;
 
     /**
      * The initial value of the component.
@@ -42,9 +113,11 @@
         <slot name="label"/>
     </div>
     <div class="container">
+        <!-- svelte-ignore a11y-autofocus -->
         <input bind:value use:forward type='password'
-            use:validate={{ invalid, error, errorMessage }}
-            {...$$restProps}/>
+            {autocomplete} {autofocus} {form} {list} {maxlength} {minlength}
+            {name} {placeholder} {readonly} {required}
+            use:validate={{ invalid, error, errorMessage }}/>
     </div>
     <div class="hint" class:invalid={$errorMessage}>
         {#if $errorMessage}
