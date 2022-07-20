6e3c9efb118c723a75aabc4f27460a247a26b8be
kwangure <kaf.wangure@gmail.com>
2022-07-20 03:34:58 +0300

    feat!: remove dropdown link component

--- src/lib/components/dropdown/index.js
+++ src/lib/components/dropdown/index.js
@@ -1,6 +1,5 @@
 import Dropdown from './dropdown.svelte';
 import Item from './item.svelte';
-import Link from './link.svelte';
 
 export default Dropdown;
-export { Item, Link };
+export { Item };
--- src/routes/components/dropdown.svelte
+++ src/routes/components/dropdown.svelte
@@ -1,5 +1,5 @@
 <script>
-    import Dropdown, { Item, Link } from '$lib/components/dropdown';
+    import Dropdown, { Item } from '$lib/components/dropdown';
     import Button from '$lib/components/button';
     import Component from '$lib/internal/component.svelte';
     import { docs } from '$lib/components/dropdown/docs';
@@ -15,16 +15,6 @@
             <Item>Penguin.</Item>
             <Item>Has.</Item>
             <Item>Dropped.</Item>
-        </Dropdown>
-
-        <Dropdown>
-            <Button slot="button">
-                Click for linked dropdown
-            </Button>
-            <Link href="#click">Click.</Link>
-            <Link href="#to">To.</Link>
-            <Link href="#change">Change.</Link>
-            <Link href="#hash">Hash.</Link>
         </Dropdown>
     </svelte:fragment>
 </Component>
