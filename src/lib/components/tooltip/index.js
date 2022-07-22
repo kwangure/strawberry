c5818eda618ec1f3b4d8022b98fcae8952e1166e
kwangure <kaf.wangure@gmail.com>
2022-07-22 04:21:44 +0300

    feat!: delete svg tooltip

--- src/lib/components/tooltip/index.js
+++ src/lib/components/tooltip/index.js
@@ -1,5 +1,3 @@
-import SVGTooltip from './svg_tooltip.svelte';
 import Tooltip from './tooltip.svelte';
 
 export default Tooltip;
-export { SVGTooltip };
--- src/lib/default/tooltip/index.js
+++ src/lib/default/tooltip/index.js
@@ -1,8 +1,6 @@
 import './index.css';
 import '../../css/reset.css';
 import '../../css/globals.css';
-import SVGTooltip from '$lib/components/tooltip/svg_tooltip.svelte';
 import Tooltip from '$lib/components/tooltip/tooltip.svelte';
 
 export default Tooltip;
-export { SVGTooltip };
