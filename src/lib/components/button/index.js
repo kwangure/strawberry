1064722515daacc9bfc966d7e2dc8385b6d1494b
kwangure <kaf.wangure@gmail.com>
2022-07-24 03:54:29 +0300

    refactor: move primary to theme folder

--- src/lib/components/button/index.js
+++ src/lib/components/button/index.js
@@ -1,5 +1,3 @@
 import Button from './button.svelte';
-import Primary from './primary.svelte';
 
 export default Button;
-export { Primary };
--- src/lib/default/button/index.js
+++ src/lib/default/button/index.js
@@ -2,7 +2,7 @@ import './index.css';
 import '../../css/reset.css';
 import '../../css/globals.css';
 import Button from '$lib/components/button/button.svelte';
-import Primary from '$lib/components/button/primary.svelte';
+import Primary from './primary.svelte';
 
 export default Button;
 export { Primary };
