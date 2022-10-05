43104333b7bda239d2fc158ccf609ff9dd22e93b
kwangure <kaf.wangure@gmail.com>
2022-10-05 21:51:52 +0300

    feat!: delete modal component

--- src/lib/docs/modal/index.js
+++
@@ -1,7 +0,0 @@
-// @ts-expect-error
-// eslint-disable-next-line import/no-unresolved
-import modalDocs from '../../components/modal/modal.svelte:docs';
-
-export const docs = {
-	main: modalDocs,
-};
