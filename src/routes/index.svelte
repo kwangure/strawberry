fa2be80f0059d740b1cdf1fc6110818c81db082a
kwangure <kaf.wangure@gmail.com>
2022-11-03 01:28:11 +0300

    feat: nuke landing page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,93 +1,32 @@
 <script>
-    import { Logo, Navbar, Section } from '$lib/default/navbar';
-    import { base } from '$app/paths';
-</script>
+    import { Code, mixedHTML } from '$lib/default/code';
+    /* eslint-disable no-useless-escape */
+    const usage = `
+<script\>
+    import { Button } from "@kwangure/strawberry/default/button";
 
-<svelte:body data-color-scheme="dark"></svelte:body>
+    const hello = () => alert("strawberry");
+</script\>
 
-<div class="layout">
-    <Navbar >
-        <Logo href="{base}/">
-            🍓 <span class="text">strawberry</span>
-        </Logo>
-        <Section --br-navbar-section-margin-inline="auto 10px">
-            <a class="cta" href="https://github.com/kwangure/strawberry" target="_blank">
-                GitHub
-            </a>
-            <a class="cta" href="https://www.npmjs.com/package/@kwangure/strawberry" target="_blank">
-                NPM
-            </a>
-        </Section>
-    </Navbar>
-    <main>
-        <h1>Strawberry</h1>
-        Strawberry is a set of Svelte components.
-
-        <div class="link">
-            <h3>
-                <a href="{base}/components">Components</a>
-            </h3>
-            Documentation on how to use Strawberry components.
-        </div>
-    </main>
+<div class="element">
+    <Button on:click={hello}>Click me!</Button>
 </div>
 
 <style>
-    .layout {
-        --nav-height: 64px;
-        --page-max-width: 56rem;
+    /* Override imported preset values */
+    .element {
+        --br-button-root-width: 50px;
+        --br-button-root-border: 3px solid turquoise;
     }
-    .layout {
-        display: contents;
-    }
-    .layout {
-        --br-navbar-root-max-width: var(--page-max-width);
-        --br-navbar-root-height: var(--nav-height);
-    }
-    main {
-        max-width: var(--page-max-width);
-        height: calc(100% - var(--nav-height));
-        margin: auto;
-        padding-inline: 8px;
+</style\>
+    `.trim();
+    /* eslint-enable no-useless-escape */
+</script>
 
-        /* Prevent heading margin collapse */
-        display: flow-root;
-    }
-    .layout {
-        --br-navbar-root-width: var(--page-max-width);
-        --br-navbar-root-margin-inline: auto;
-    }
-    @media (max-width: 56rem) {
-        .layout {
-            --br-navbar-root-width: auto;
-        }
-    }
-    h3 {
-        margin-top: 0px;
-    }
-    .link {
-        position: relative;
-        padding: 16px;
-        border-radius: 4px;
-        margin-top: 15px;
-    }
-    @media (prefers-color-scheme: dark) {
-        .link {
-            border: 1px solid #474b50;
-        }
-    }
-    @media (prefers-color-scheme: light) {
-        .link {
-            border: 1px solid #c3c3c3;
-        }
-    }
-    .link a:before {
-        display: block;
-        inset: 0px;
-        position: absolute;
-        content: "";
-    }
-    .cta {
-        line-height: var(--br-navbar-root-height);
-    }
-</style>
+<h1>Installation</h1>
+
+A set of Svelte components.
+
+Where possible, a Strawberry component is simply a light wrapper around a built-in element, making it easier to use it the Svelte way.
+
+<Code code='npm install @kwangure/strawberry'/>
