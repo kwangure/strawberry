442942caa13950d19877b117cdd33a1afbe5c0f9
kwangure <kaf.wangure@gmail.com>
2022-11-05 00:49:07 +0300

    docs: update with latest date component

--- src/routes/input/date.svelte
+++ src/routes/input/date.svelte
@@ -1,34 +1,14 @@
 <script>
-    import { Code, plainHTML } from '$lib/default/code';
     import Component from '$lib/internal/component.svelte';
+    import { Container } from '$lib/default/input/container';
     import { Date } from '$lib/default/input/date';
     import { docs } from '$lib/docs/input/date';
 </script>
 
 <Component {docs}>
     <svelte:fragment slot="inline">
-        <Date value="2017-06-01"/>
-    </svelte:fragment>
-    <svelte:fragment slot="accessibility">
-        You should always include the label text for the input for the benefit
-        of screen-reader users. You may use custom-properties to visually
-        hide it if you must. For example:
-        <Code highlight={plainHTML} code={`
-<div class="element">
-    <Label>
-        Birthday
-        <Date slot='input'/>
-    </Label>
-</div>
-
-<style>
-    .element {
-        --br-input-label-root-position: absolute;
-        --br-input-label-root-height: 1px;
-        --br-input-label-root-width: 1px;
-        --br-input-label-root-overflow: hidden;
-    }
-</style>
-`}/>
+        <Container>
+            <Date value="2017-06-01"/>
+        </Container>
     </svelte:fragment>
 </Component>
