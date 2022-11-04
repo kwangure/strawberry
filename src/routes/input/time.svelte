c925ea573a2c361ad70ecde4c186f0f82124b85d
kwangure <kaf.wangure@gmail.com>
2022-11-05 02:47:57 +0300

    feat!: separate time from container

--- src/routes/input/time.svelte
+++ src/routes/input/time.svelte
@@ -1,34 +1,14 @@
 <script>
-    import { Code, plainHTML } from '$lib/default/code';
     import Component from '$lib/internal/component.svelte';
+    import { Container } from '$lib/default/input/container';
     import { docs } from '$lib/docs/input/time';
     import { Time } from '$lib/default/input/time';
 </script>
 
 <Component {docs}>
     <svelte:fragment slot="inline">
-        <Time value={new Date().toISOString()}/>
-    </svelte:fragment>
-    <svelte:fragment slot="accessibility">
-        You should always include the label text for the input for the benefit
-        of screen-reader users. You may use custom-properties to visually
-        hide it if you must. For example:
-        <Code highlight={plainHTML} code={`
-<div class="element">
-    <Label>
-        Delivery time
-        <Time slot='input'/>
-    </Label>
-</div>
-
-<style>
-    .element {
-        --br-input-label-root-position: absolute;
-        --br-input-label-root-height: 1px;
-        --br-input-label-root-width: 1px;
-        --br-input-label-root-overflow: hidden;
-    }
-</style>
-`}/>
+        <Container>
+            <Time value={new Date().toISOString()}/>
+        </Container>
     </svelte:fragment>
 </Component>
