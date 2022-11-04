87d244270aa58b40186db54d437655d1c22a3589
kwangure <kaf.wangure@gmail.com>
2022-11-05 00:31:37 +0300

    feat!: separate datetime and container

---
+++ src/lib/components/input/container/container.svelte
@@ -0,0 +1,22 @@
+<div class="container">
+    <slot/>
+</div>
+
+<style>
+    .container {
+        display: flex;
+        border: var(--br-input-container-border);
+        border-radius: var(--br-input-container-border-radius);
+        background-color: var(--br-input-container-background-color);
+        height: var(--br-input-container-height);
+        transition: var(--br-input-container-transition);
+    }
+    .container:hover:not(:focus-within) {
+        box-shadow: var(--br-input-container-hover-box-shadow);
+        border: var(--br-input-container-hover-border);
+    }
+    .container:focus-within {
+        box-shadow: var(--br-input-container-focus-box-shadow);
+        border: var(--br-input-container-focus-border);
+    }
+</style>
