0c761640c391dd5b51090d4e35cf969e95b7fe25
kwangure <kaf.wangure@gmail.com>
2022-11-25 07:28:23 +0300

    refactor: remove unused docs

--- src/lib/docs/code/index.js
+++
@@ -1,7 +0,0 @@
-// @ts-expect-error
-// eslint-disable-next-line import/no-unresolved
-import codeDocs from '../../components/code/code.svelte:docs';
-
-export const docs = {
-	main: codeDocs,
-};
--- src/lib/docs/dialog/index.js
+++
@@ -1,7 +0,0 @@
-// @ts-expect-error
-// eslint-disable-next-line import/no-unresolved
-import dialogDocs from '../../components/dialog/dialog.svelte:docs';
-
-export const docs = {
-	main: dialogDocs,
-};
