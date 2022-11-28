16c9fe53dd14cf2bcc33842db31a1aed9f864142
kwangure <kaf.wangure@gmail.com>
2022-11-28 06:42:25 +0300

    feat: prefer style-based dropdown item

--- src/lib/components/dropdown/index.js
+++ src/lib/components/dropdown/index.js
@@ -1,4 +1,3 @@
 export { default as Arrow } from './arrow.svelte';
 export { default as Dropdown } from './dropdown.svelte';
-export { default as Item } from './item.svelte';
 export { default as Popup } from './popup.svelte';
