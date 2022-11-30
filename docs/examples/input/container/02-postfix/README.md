501168b3dd48ebce9513149384cf459f016edf78
kwangure <kaf.wangure@gmail.com>
2022-11-30 20:41:13 +0300

    docs: document input postfix

---
+++ docs/examples/input/container/02-postfix/README.md
@@ -0,0 +1,8 @@
+---
+{
+	"title": "Postfix container"
+}
+---
+
+The number input provides a `Postfix` element to enable stepping the input
+value using a pointer.
---
+++ docs/examples/input/container/02-postfix/component.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { Container } from '@kwangure/strawberry/default/input/container';
+	import { Postfix } from '@kwangure/strawberry/default/input/number';
+	import { validate } from '@kwangure/strawberry/actions/validate.js';
+</script>
+
+<Container let:inputId>
+	<label for={inputId} slot="label">
+		Use the postfix to step through numbers
+	</label>
+	<input id={inputId} placeholder="3.14"
+		required type='number' use:validate/>
+	<Postfix/>
+</Container>
