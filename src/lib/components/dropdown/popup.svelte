9bd28aa5c3ff55ed510dfff82204b6c5b74163e1
kwangure <kaf.wangure@gmail.com>
2022-12-01 15:57:31 +0300

    fix: actually check popup visibility

--- src/lib/components/dropdown/popup.svelte
+++ src/lib/components/dropdown/popup.svelte
@@ -11,7 +11,11 @@
 		padding-inline: var(--br-dropdown-popup-padding-inline);
         background-color: var(--br-dropdown-popup-background-color);
         border-radius: var(--br-dropdown-popup-border-radius);
-        visibility: var(--br-dropdown-popup-show, --br-dropdown-popup-visibility);
+        /*
+            pop-show is either `hidden` or ' '. When ' ' to mean show we should
+            show, check that popup-visibility is visible.
+        */
+        visibility: var(--br-dropdown-popup-show, var(--br-dropdown-popup-visibility));
         gap: var(--br-dropdown-popup-gap);
         display: flex;
         flex-direction: column;
