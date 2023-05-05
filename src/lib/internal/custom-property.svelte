6a969b3a2939784705ff7e855eb0738fdbeb6628
kwangure <kaf.wangure@gmail.com>
2023-05-05 04:07:11 +0300

    docs: rework custom property docs

--- src/lib/internal/custom-property.svelte
+++ src/lib/internal/custom-property.svelte
@@ -5,7 +5,6 @@
     import { Dropdown, Popup } from '$lib/default/dropdown';
     import { mdiDotsHorizontal, mdiLinkVariant } from '@mdi/js';
     import { css } from '$lib/code';
-    import { Icon } from './icon';
 
     /** @type {any} */
     export let filepath;
@@ -18,24 +17,26 @@
 <div class='property'>
     <a href="#{customProperty}" class="hash">
         <button>
-            <Icon path={mdiLinkVariant}/>
+            <svg viewBox="0 0 24 24">
+                <path d={mdiLinkVariant} fill="currentColor"/>
+            </svg>
         </button>
     </a>
-    <h3 id={customProperty}>
-        <code>
-            {#each css(customProperty) as { segment, color }}
-                {#if color}
-                    <span style='color: var(--br-code-token-{color}-color);'>{segment}</span>
-                {:else}
-                    {segment}
-                {/if}
-            {/each}
-        </code>
-    </h3>
+    <code id={customProperty}>
+        {#each css(customProperty) as { segment, color }}
+            {#if color}
+                <span style='color: var(--br-code-token-{color}-color);'>{segment}</span>
+            {:else}
+                {segment}
+            {/if}
+        {/each}
+    </code>
     {#if start}
         <Dropdown>
             <button slot="trigger">
-                <Icon path={mdiDotsHorizontal}/>
+                <svg viewBox="0 0 24 24">
+                    <path d={mdiDotsHorizontal} fill="currentColor"/>
+                </svg>
             </button>
             <Popup>
                 <a class='item' href={mdnUrl} target="_blank" title="Opens in new tab" rel=noreferrer>
@@ -56,14 +57,6 @@
                         View on Github
                     </div>
                 </a>
-                <a class='item' href="https://github.dev/kwangure/strawberry/blob/master/{filepath}#L{start.line}" target="_blank" rel=noreferrer title="Opens in new tab">
-                    <div class="br-focusable">
-                        <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img">
-                            <path clip-rule="evenodd" d="M50 5.7C24.9 5.7 4.5 26 4.6 51.2c0 19.6 12.5 36.9 31.1 43.1 2.3.4 3.1-1 3.1-2.2s0-3.9-.1-7.7c-12.6 2.7-15.3-6.1-15.3-6.1-2.1-5.3-5-6.7-5-6.7-4.1-2.9.3-2.8.3-2.8 4.6.3 7 4.7 7 4.7 4.1 6.9 10.6 4.9 13.2 3.8.2-2.3 1.2-4.5 2.9-6.1-10.2-1.1-20.8-5-20.8-22.5-.1-4.5 1.6-8.9 4.7-12.2-.5-1.1-2-5.7.4-12 0 0 3.8-1.2 12.5 4.7 7.4-2 15.3-2 22.8 0 8.7-5.9 12.4-4.7 12.4-4.7 2.5 6.3 1 10.9.5 12 3.1 3.3 4.8 7.7 4.7 12.2 0 17.5-10.6 21.4-20.8 22.4 1.6 1.4 3.1 4.2 3.1 8.4 0 6.1-.1 11-.1 12.4 0 1.2.8 2.6 3.1 2.2 23.8-8 36.6-33.8 28.6-57.6C86.8 18.2 69.5 5.7 50 5.7z" fill="currentColor" fill-rule="evenodd"/>
-                        </svg>
-                        View in Github Web Editor
-                    </div>
-                </a>
             </Popup>
         </Dropdown>
     {/if}
@@ -79,38 +72,32 @@
         display: flex;
         align-items: center;
         margin-left: -34px;
+        gap: var(--br-size-2);
     }
-    .property {
-        --br-button-root-border: none;
-        --br-button-root-hover-border: none;
-        --br-button-background-color: transparent;
-        --br-button-hover-background-color: transparent;
-        --br-button-font-color: transparent;
-        --br-button-root-padding-inline: 5px;
-        --br-link-font-color: var(--br-font-color);
-        --br-icon-root-height: 20px;
+    .property button {
+        border: none;
+        transition: all 0.25s ease;
+        background-color: transparent;
     }
-    .property:hover {
-        --br-button-font-color: inherit;
-        --br-button-transition: all 0.25s ease;
-    }
-    h3 {
-        margin: 0;
-    }
-    h3:target {
-        animation: highlight 2s;
+    .property:not(:hover) button {
+        color: transparent;
     }
     @keyframes highlight {
         from {
-            --br-code-root-background-color: yellow;
-            --br-code-root-font-color: black;
+            background-color: var(--br-dark, #444) var(--br-light, hsl(215deg 100% 90%));
         }
         to {
-            --br-code-root-background-color: yellow;
-            --br-code-root-font-color: black;
+            background-color: var(--br-dark, #444) var(--br-light, hsl(215deg 100% 90%));
         }
     }
     svg {
         height: 16px;
+    }
+    code {
+        padding: 0px;
+        font-size: inherit;
+    }
+    code:target {
+        animation: highlight 2s;
     }
 </style>
--- src/lib/internal/icon/index.js
+++
@@ -1,2 +0,0 @@
-export { default as Icon } from './icon.svelte';
-export { default as Path } from './path.svelte';
